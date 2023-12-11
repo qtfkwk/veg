ea6bc896548db7bf744bc88ec7c7d8974f12b953
qtfkwk <qtfkwk+veg@gmail.com>
2023-12-11 17:02:52 -0500

    0.1.0

---
+++ README.md
@@ -0,0 +1,77 @@
+```
+// Create a custom type
+
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+// Implement a method that creates a Box of the custom type
+
+impl Point {
+    fn new(x: f32, y: f32) -> Box<Point> {
+        Box::new(Point { x, y })
+    }
+}
+
+// Implement the veg::Table::row method to define how to print the custom type
+
+impl veg::Table for Point {
+    fn row(&self) -> Vec<String> {
+        // Add `$`
+        [self.x, self.y].iter().map(|x| format!("${x}$")).collect()
+
+        // Other ideas:
+
+        // - Add 3 decimal places:
+        //
+        //[self.x, self.y].iter().map(|x| format!("${x:.3}$")).collect()
+
+        // - Do something different for x and y:
+        //
+        // vec![
+        //    format!("${:.1}$", self.x),
+        //    format!("${:.4}$", self.y),
+        //]
+
+        // - Just convert to string:
+        //
+        //[self.x, self.y].iter().map(|x| x.to_string())).collect()
+
+        // ...
+    }
+}
+
+// Create a Veg via the table function with a header definition
+
+let mut t = veg::table("$x$|$y$\n---:|---:");
+
+// Add a single point
+
+t.push(Point::new(1.0, 1.0));
+
+// Add a bunch of points
+
+t.append(&mut vec![
+    Point::new(2.0, 4.0),
+    Point::new(3.0, 9.0),
+    Point::new(4.0, 16.0),
+]);
+
+// Render as markdown
+
+let markdown = t.markdown();
+
+assert_eq!(
+    markdown,
+    "  \
+  $x$ |  $y$
+-----:|-----:
+  $1$ |  $1$
+  $2$ |  $4$
+  $3$ |  $9$
+  $4$ | $16$
+\
+    ",
+);
+```
---
+++ src/lib.rs
@@ -0,0 +1,89 @@
+#![doc = include_str!("../README.md")]
+
+pub trait Table {
+    fn row(&self) -> Vec<String>;
+}
+
+pub struct Veg {
+    header: String,
+    rows: Vec<Box<dyn Table>>,
+}
+
+impl Veg {
+    pub fn new(header: &str) -> Veg {
+        Veg {
+            header: header.into(),
+            rows: vec![],
+        }
+    }
+
+    pub fn push(&mut self, item: Box<dyn Table>) {
+        self.rows.push(item);
+    }
+
+    pub fn append(&mut self, other: &mut Vec<Box<dyn Table>>) {
+        self.rows.append(other);
+    }
+
+    pub fn markdown(&self) -> String {
+        let mut r = header(&self.header);
+        r.append(&mut self.rows.iter().map(|x| x.row()).collect::<Vec<_>>());
+
+        let right = r[1]
+            .iter()
+            .enumerate()
+            .filter_map(|(i, x)| if x.ends_with(':') { Some(i) } else { None })
+            .collect::<Vec<_>>();
+
+        let mut width = r[0].iter().map(|_| 0).collect::<Vec<_>>();
+        for row in r.iter() {
+            for (col, cell) in row.iter().enumerate() {
+                width[col] = width[col].max(cell.chars().collect::<Vec<_>>().len() + 1);
+            }
+        }
+
+        let mut r = r
+            .iter()
+            .enumerate()
+            .map(|(i, x)| {
+                if i == 1 {
+                    x.iter()
+                        .map(|x| format!("--{x}"))
+                        .collect::<Vec<_>>()
+                        .join("|")
+                } else {
+                    x.iter()
+                        .enumerate()
+                        .map(|(i, x)| {
+                            if right.contains(&i) {
+                                format!("{x:>0$}", width[i])
+                            } else {
+                                format!("{x:<0$}", width[i])
+                            }
+                        })
+                        .collect::<Vec<_>>()
+                        .join(" |")
+                }
+            })
+            .collect::<Vec<_>>()
+            .join("\n");
+
+        r.push('\n');
+
+        r
+    }
+}
+
+fn header(s: &str) -> Vec<Vec<String>> {
+    s.split('\n')
+        .map(|x| {
+            x.split("|")
+                .map(|x| x.trim().to_string())
+                .collect::<Vec<_>>()
+        })
+        .collect()
+}
+
+pub fn table(header: &str) -> Veg {
+    Veg::new(header)
+}
